d0005c9a0abd070bbdcf619069e301fe244ef4bd
Ethan Joh <ethan.joh@gmail.com>
2016-03-16 00:05:56 +0900

    어드민 메뉴명 수정

--- admin/include/admin_sidebar.php
+++ admin/include/admin_sidebar.php
@@ -50,8 +50,8 @@
 	                      <ul class="sub">
 	                          <li><a  href="/admin/member/top_member_list.php">기업회원 리스트</a></li>
 	                          <li><a  href="/admin/member/p_top_member_list.php">개인회원 리스트</a></li>
-	                          <li><a  href="/admin/member/mem_sendmail_list.php" target="_blank">전체메일 발송</a></li>
-	                          <li><a  href="/admin/member/mem_ins_member.php">회원 등록</a></li>
+	                          <li><a  href="/admin/member/mem_sendmail_list.php" target="_blank">기업회원 전체메일 발송</a></li>
+	                          <li><a  href="/admin/member/mem_ins_member.php">기업회원 등록</a></li>
 	                      </ul>
 	                  </li>
 <!--
