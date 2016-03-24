4b79d891a55a663f2b3737ed338662ce68bf5eca
Ethan Joh <ethan.joh@gmail.com>
2016-03-24 10:45:11 +0900

    상품등록

--- get-product.php
+++ get-product.php
@@ -54,7 +54,7 @@ while ($data = fgetcsv($fp)) {
     // print_r($data);
     // echo "</pre>";
 
-    $lcode          = "9";                           // 대카테고리
+    $lcode          = "11";                          // 대카테고리
     $itemCode       = trim($data['1']);              // 상품코드
     $shortDesc      = trim($data['8']);              // 규격=>간략설명
     $wholesalePrice = removeComma(trim($data['3'])); // 공급가. 기존 소비자가 컬럼에 삽입
@@ -72,7 +72,7 @@ while ($data = fgetcsv($fp)) {
     if (trim($data['5']) == '아이언/우드그립' || trim($data['5']) == '클럽그립' || trim($data['5']) == '주니어 그립') {
         // 이미지 파일이 있는 경우
         if ($data['4'] != '') {
-            $mcode       = "15"; // 중카테고리
+            $mcode       = "17"; // 중카테고리
             $bImg1_chk   = "Y";
             $bigImg1File = $saveDir . $data['6'] . "/b/" . $data['4'];
 
