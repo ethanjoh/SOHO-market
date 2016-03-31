d5dd986f41f9b340795b1b4344b4f181b8f94812
Ethan Joh <ethan.joh@gmail.com>
2016-03-31 21:23:23 +0900

    브랜드 문구 수정

--- admin/category/top_ca_list.php
+++ admin/category/top_ca_list.php
@@ -17,10 +17,10 @@
 
 <?php
 
-// 상위카테고리 코드값으로 부터 현 카테고리 값을 구함
-$query       = "SELECT * FROM products_category1 ORDER BY code";
-$result      = mysqli_query($connect, $query);
-$total_count = mysqli_num_rows($result);
+    // 상위브랜드 코드값으로 부터 현 브랜드 값을 구함
+    $query       = "SELECT * FROM products_category1 ORDER BY code";
+    $result      = mysqli_query($connect, $query);
+    $total_count = mysqli_num_rows($result);
 ?>
 
             <!-- info start-->
@@ -31,11 +31,11 @@ $total_count = mysqli_num_rows($result);
                     사용방법
                   </header>
                   <ul class="info-body">
-                    <li><i class="fa fa-info-circle"></i> 하단의 카테고리 등록하기를 통해 등록하세요.</li>
-                    <li><i class="fa fa-info-circle"></i> 카테고리 등록 후 중분류 등록은 해당 카테고리명을 클릭하세요.</li>
+                    <li><i class="fa fa-info-circle"></i> 하단의 브랜드 등록하기를 통해 등록하세요.</li>
+                    <li><i class="fa fa-info-circle"></i> 브랜드 등록 후 중분류 등록은 해당 브랜드명을 클릭하세요.</li>
                     <li><i class="fa fa-info-circle"></i> 등록 후에도 수정이 가능합니다.</li>
                     <li><i class="fa fa-info-circle"></i> 메인메뉴에서 감추려면 '숨김'을 누르세요.</li>
-                    <li><i class="fa fa-info-circle"></i> <i class="fa fa-external-link"></i>를 클릭하면 새 창에서 해당 카테고리가 열립니다.</li>
+                    <li><i class="fa fa-info-circle"></i> <i class="fa fa-external-link"></i>를 클릭하면 새 창에서 해당 브랜드가 열립니다.</li>
                   </ul>
                 </section>
               </div>
@@ -47,7 +47,9 @@ $total_count = mysqli_num_rows($result);
               <div class="col-sm-12">
                 <section class="panel">
                   <header class="panel-heading table-head">
-                      카테고리 목록 (총 <?php echo $total_count; ?> 개)
+                      브랜드 목록 (총
+                      <?php echo $total_count; ?>
+                      개)
                   </header>
                   <div class="panel-body">
 
@@ -57,8 +59,8 @@ $total_count = mysqli_num_rows($result);
                         <thead>
                           <tr>
                             <th>코드</th>
-                            <th>카테고리명</th>
-                            <th>하위 중분류 수</th>
+                            <th>브랜드명</th>
+                            <th>하위 분류 갯수</th>
                             <th>등록 상품수</th>
                             <th>숨기기</th>
                             <th>관리</th>
@@ -67,17 +69,17 @@ $total_count = mysqli_num_rows($result);
                         <tbody>
 <?php
 
-for ($i = 0; $row = mysqli_fetch_array($result); $i++) {
+    for ($i = 0; $row = mysqli_fetch_array($result); $i++) {
 
-    $query     = "SELECT * FROM products_category2 WHERE up_category='$row[code]'";
-    $result2   = mysqli_query($connect, $query);
-    $sub_count = mysqli_num_rows($result2);
-    mysqli_free_result($result2);
+        $query     = "SELECT * FROM products_category2 WHERE up_category='$row[code]'";
+        $result2   = mysqli_query($connect, $query);
+        $sub_count = mysqli_num_rows($result2);
+        mysqli_free_result($result2);
 
-    $query1         = "SELECT * FROM products WHERE category_l='$row[code]'";
-    $result3        = mysqli_query($connect, $query1);
-    $products_count = mysqli_num_rows($result3);
-    mysqli_free_result($result3);
+        $query1         = "SELECT * FROM products WHERE category_l='$row[code]'";
+        $result3        = mysqli_query($connect, $query1);
+        $products_count = mysqli_num_rows($result3);
+        mysqli_free_result($result3);
 
     ?>
                           <tr>
@@ -90,11 +92,11 @@ for ($i = 0; $row = mysqli_fetch_array($result); $i++) {
                             <td>
 <?php
 
-    if ($row['hide'] == "Y") {
-        echo "<a type=\"button\" class=\"btn btn-round btn-success\" href='ca_hide.php?code=" . $row['code'] . "&amp;chk=Y'><i class=\"fa fa-times\"></i> ON</a>";
-    } else {
-        echo "<a type=\"button\" class=\"btn btn-round btn-default\" href='ca_hide.php?code=" . $row['code'] . "&amp;chk=N'><i class=\"fa fa-check\"></i> OFF</a>";
-    }
+        if ($row['hide'] == "Y") {
+            echo '<a type="button" class="btn btn-round btn-success" href="ca_hide.php?code=' . $row['code'] . '&amp;chk=Y"><i class="fa fa-times"></i> ON</a>' . "\r\n";
+        } else {
+            echo '<a type="button" class="btn btn-round btn-default" href="ca_hide.php?code=' . $row['code'] . '&amp;chk=N"><i class="fa fa-check"></i> OFF</a>' . "\r\n";
+        }
     ?>
                             </td>
                             <td>
@@ -103,18 +105,17 @@ for ($i = 0; $row = mysqli_fetch_array($result); $i++) {
                             </td>
                           </tr>
 <?php
+    } // end of for loop
 
-} // end of for loop
+    mysqli_free_result($result);
 
-mysqli_free_result($result);
-
-if ($total_count == 0) {
+    if ($total_count == 0) {
     ?>
                           <tr>
-                            <td colspan="5" class="text-center"><p>등록된 카테고리가 없습니다.</p></td>
+                            <td colspan="5" class="text-center"><p>등록된 브랜드가 없습니다.</p></td>
                           </tr>
-                  <?php
-}
+<?php
+    }
 ?>
                         </tbody>
                       </table>
@@ -129,7 +130,7 @@ if ($total_count == 0) {
             <!-- buttons start -->
             <div class="row text-center">
               <div class="col-sm-12">
-                <a type="button" class="btn btn-success" href="ca_register.php">카테고리 등록하기</a>
+                <a type="button" class="btn btn-success" href="ca_register.php">브랜드 등록하기</a>
               </div>
             </div>
             <!-- buttons end -->
