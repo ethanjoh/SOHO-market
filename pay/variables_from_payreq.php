c5be5e2fe81ed7233923cb6dba6b94d87044b851
Ethan Joh <ethan.joh@gmail.com>
2020-03-21 23:23:36 +0900

    취소 시 재고복구 수정(사용자페이지)

--- pay/variables_from_payreq.php
+++ pay/variables_from_payreq.php
@@ -123,22 +123,21 @@ if ($result) {
         }
 
         $s_tot     = (int) $rows['volume'] * (int) $rows['amount']; // 소계 = volume(주문수량) X amount(단가)
-        $tot_money = $tot_money + $s_tot; //총합
-        //$products_stock = $rows['stock'] - $rows['volume']; // 제품 전체재고에서 카트 재고 뺌
+        $tot_money = $tot_money + $s_tot;                           //총합
+                                                                    //$products_stock = $rows['stock'] - $rows['volume']; // 제품 전체재고에서 카트 재고 뺌
 
         $products_num[$i]   = $rows['num'];
         $products_name[$i]  = stripslashes($rows['name']);
         $products_price[$i] = calc_offer_price($rows['retail_price'], $p_id); // 업체별 공급가 확인
-        $products_opt       = explode(",", $rows['opt']); // 제품의 옵션을 배열로 저장
-        $products_opt_count = explode(",", $rows['opt_count']); // 제품의 옵션수량을 배열로 저장
-        $products_opt_stock = explode(",", $rows['opt_stock']); // 제품의 품절표시 배열로 저장
-        $order_opt[$i]      = $rows['p_opt']; // 카트에 담긴 옵션명
-        $order_count[$i]    = $rows['volume']; // 카트에 담긴 옵션 수량
+        $products_opt       = explode(",", $rows['opt']);                     // 제품의 옵션을 배열로 저장
+        $products_opt_count = explode(",", $rows['opt_count']);               // 제품의 옵션수량을 배열로 저장
+        $products_opt_stock = explode(",", $rows['opt_stock']);               // 제품의 품절표시 배열로 저장
+        $order_opt[$i]      = $rows['p_opt'];                                 // 카트에 담긴 옵션명
+        $order_count[$i]    = $rows['volume'];                                // 카트에 담긴 옵션 수량
 
         // 옵션별 재고 업데이트
         for ($j = 0; $j < sizeof($products_opt); $j++) {
             if ($products_opt[$j] == $order_opt[$i]) {
-                // $new_opt_count[$j] = $products_opt_count[$j] - $order_count[$i]; // 전체재고에서 주문수량 차감
                 $products_opt_count[$j] -= $order_count[$i]; // 전체재고에서 주문수량 차감
                 $final_opt_count[$j] = $products_opt_count[$j];
 
--- shop/order-delete.php
+++ shop/order-delete.php
@@ -31,14 +31,13 @@ $xpay->Set("LGD_TID", $LGD_TID);
  * 주문취소 처리
  */
 //주문 취소에 따른 재고 복구
-$qry = "SELECT * FROM mall_order WHERE num = '$oid' ";
+$qry = "SELECT * FROM mall_order WHERE num = '2887' ";
 $res = mysqli_query($connect, $qry);
 $row = mysqli_fetch_array($res);
 
-$a_goods_fk     = explode(",", $row['goods_fk']); //상품 코드
-$a_goods_kind   = explode(",", $row['goods_kind']); //주문상품 옵션
-$a_goods_volume = explode(",", $row['goods_count']); //변경된 수량
-$new_opt_count  = array();
+$a_goods_fk     = explode(",", $row['goods_fk']);    //상품 코드
+$a_goods_kind   = explode(",", $row['goods_kind']);  //주문상품 옵션
+$a_goods_volume = explode(",", $row['goods_count']); // 주문 수량
 
 for ($i = 0; $i < sizeof($a_goods_fk); $i++) {
     $pro_sql    = "SELECT * FROM products WHERE num = '$a_goods_fk[$i]' ";
@@ -48,17 +47,27 @@ for ($i = 0; $i < sizeof($a_goods_fk); $i++) {
     /**
     취소 옵션 재고 업데이트
      **/
-    // 주문제품의 옵션을 가져옴
-    $products_opt       = explode(",", $pro_row['opt']); // 제품의 옵션을 배열로 저장
+                                                               // 주문제품의 옵션을 가져옴
+    $products_opt       = explode(",", $pro_row['opt']);       // 제품의 옵션을 배열로 저장
     $products_opt_count = explode(",", $pro_row['opt_count']); // 제품의 옵션을 배열로 저장
+    $opt_del_chk        = explode(",", $pro_row['opt_stock']);
 
     // 옵션별 재고 업데이트
     for ($j = 0; $j < sizeof($products_opt); $j++) {
         if ($products_opt[$j] == $a_goods_kind[$i]) {
-            $products_opt_count[$j] = $products_opt_count[$j] + $a_goods_volume[$i]; // 취소 주문수량 가감 후 전체재고 업데이트
-            $final_opt_count        = implode(",", $products_opt_count);
+            $products_opt_count[$j] += $a_goods_volume[$i]; // 취소 주문수량 가감 후 전체재고 업데이트
+            $final_opt_count = implode(",", $products_opt_count);
 
-            $qry2 = "UPDATE products SET opt_count='$final_opt_count' WHERE num = '$a_goods_fk[$i]' ";
+            if ($opt_del_chk[$j] == "0" || "-1") {
+                $opt_del_chk[$j] = "1";
+                $final_del_chk   = implode(",", $opt_del_chk);
+            }
+
+            echo "<pre>";
+            print_r($final_del_chk);
+            echo "</pre>";
+
+            $qry2 = "UPDATE products SET opt_count='$final_opt_count', opt_stock='$final_del_chk' WHERE num = '$a_goods_fk[$i]' ";
             mysqli_query($connect, $qry2);
         }
     }
@@ -77,10 +86,7 @@ if ($xpay->TX()) {
     $LGD_RESPCODE = $xpay->Response_Code();
     $LGD_RESPMSG  = $xpay->Response_Msg();
 
-    $query2 = "UPDATE pg_info SET
-                                        LGD_RESPCODE            = '$LGD_RESPCODE',
-                                        LGD_RESPMSG             = '$LGD_RESPMSG'
-                                    WHERE LGD_TID = '$LGD_TID'";
+    $query2 = "UPDATE pg_info SET LGD_RESPCODE = '$LGD_RESPCODE', LGD_RESPMSG = '$LGD_RESPMSG' WHERE LGD_TID = '$LGD_TID'";
 
     $result2 = mysqli_query($connect, $query2);
 
